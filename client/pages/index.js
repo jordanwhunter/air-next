4fd6864ba231777260d0f22b6e39f09a801b1ad0
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-08-06 13:51:04 -0400

    First commit

---
+++ client/pages/index.js
@@ -0,0 +1,22 @@
+// Dependencies
+import Head from 'next/head';
+
+// Components
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Airbnb: Clone</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <Header />
+
+      {/* Banner */}
+
+      
+    </div>
+  )
+}
