6ec651bd3df618aff935b6869812d51bf8626cf8
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-08-06 14:22:54 -0400

    Left div of Header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,45 @@
+// Dependencies
 import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <header className='
+      sticky 
+      top-0 
+      z-50 
+      grid-cols-3 
+      bg-white 
+      shadow-md 
+      py-5 
+      px-5
+      md:px-10
+    '>
+
+      {/* left div */}
+      <div className='
+        relative 
+        flex 
+        items-center 
+        h-10 
+        cursor-pointer 
+        my-auto
+      '>
+        {/* Because of server-side image rendering baked into Next.js, we have to essentially whitelist the source that the images come from. This is done in the next.config.js file that we create */}
+        <Image 
+          src='https://links.papareact.com/qd3'
+          alt='Airbnb Logo'
+          layout='fill'
+          objectFit='contain'
+          objectPosition='left'
+        />
+      </div>
+
+      {/* middle div */}
+
+      {/* right div */}
+
+    </header>
   )
 }
 
