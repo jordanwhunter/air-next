d29e673d68db362abb5c541fe2647c79a5459c41
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-08-08 17:42:00 -0400

    Imported components to index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,8 +5,10 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, liveAnywhereData }) {
   // to view data pulled from api: console.log(exploreData)
   return (
     <div>
@@ -42,7 +44,7 @@ export default function Home({ exploreData }) {
             lg:grid-cols-3
             xl:grid-cols-4
           '>
-            {exploreData.map((item) => (
+            {exploreData?.map((item) => (
               <SmallCard
                 key={Math.random().toString(36).substr(2, 9)} 
                 img={item.img}
@@ -51,6 +53,43 @@ export default function Home({ exploreData }) {
               />
             ))}
           </div>
+        </section>
+
+        <section>
+          <h2 className='
+            text-4xl
+            font-semibold
+            py-8
+          '>
+            Live Anywhere
+          </h2>
+
+          <div className='
+            flex
+            space-x-3
+            overflow-scroll
+            scrollbar-hide
+            p-3
+            ml-5
+            mr-5
+          '>
+            {liveAnywhereData?.map((item) => (
+              <MediumCard 
+                key={Math.random().toString(36).substr(2, 9)}
+                img={item.img}
+                title={item.title}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <LargeCard 
+            img='https://links.papareact.com/4cj'
+            title='The Greatest Outdoors'
+            description='Wishlists curated by Airbnb'
+            buttonText='Get Inspired'
+          />
         </section>
       </main>
 
@@ -63,10 +102,14 @@ export async function getStaticProps() {
   const exploreData = await fetch('https://links.papareact.com/pyp')
   .then((res) => res.json());
 
+  const liveAnywhereData = await fetch('https://links.papareact.com/zp1')
+  .then((res) => res.json());
+
   // returns the fetched data above to the client side 
   return {
     props: {
-      exploreData: exploreData
+      exploreData: exploreData,
+      liveAnywhereData: liveAnywhereData
     }
   };
 };
